09adaed94728e7ce974e738ad52b16488156c0ea
tianfuxing <2423265571@qq.com>
2023-06-03 19:42:24 +0800

    支付接口开发

--- orderFood-java/src/main/java/com/Application.java
+++ orderFood-java/src/main/java/com/Application.java
@@ -1,7 +1,12 @@
 package com.study;
 
+import com.study.common.aspect.WebLogAspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * SpringBoot启动程序
@@ -9,9 +14,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebLogAspect.class);
 
 	public static void main(String[] args) {
 	    SpringApplication.run(Application.class,args);
+		LOGGER.info("----------启动成功------------------");
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application .class);
+	}
+
 }
--- orderfood-wx/app.js
+++ orderfood-wx/app.js
@@ -1,7 +1,7 @@
 // app.js
 App({
   globalData: {
-    apiUrl:"http://127.0.0.1:8081",
-    imageUrl:"http://127.0.0.1:8081" + "/file/"
+    apiUrl:"http://47.93.255.10:8080",
+    imageUrl:"http://47.93.255.10:8080" + "/file/"
   }
 })
